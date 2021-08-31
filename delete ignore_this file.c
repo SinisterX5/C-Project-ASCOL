0906df0ee89f5154f1e229aa864a4ddf2fcf0c4f
deadclown09 <mesandesh.2014@gmail.com>
2021-08-31 19:31:19 +0545

    Sandesh's commit

--- delete ignore_this file.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    printf("If you saw this, delete the 'ignore_this.c' file in the repository");
-    printf("Thank You!");
-    return 0;
-}
--- ignore_this.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-	printf("Just ignore it, will you? I am performing some tests with my under-beginner level skills.");
-	printf("ok");
-        return 0;
-}
